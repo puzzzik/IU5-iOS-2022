4edf790cce5875c58e80e324d583e121f89f9481
puzzzik <vtazenkovv@yandex.ru>
2022-03-18 16:28:30 +0300

    lab4 refactoring

---
+++ LABS/LAB_4/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  LABS
+//
+//  Created by Ilya Murashko on 14.02.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
