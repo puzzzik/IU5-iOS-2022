ae20a71f17abd5294c4bedb1a93c90363900ce1f
puzzzik <vtazenkovv@yandex.ru>
2022-03-20 01:33:08 +0300

    lab4 refactoring

--- LABS/LAB_4/InitialViewController/InitialViewController.swift
+++ LABS/LAB_4/InitialViewController/InitialViewController.swift
@@ -11,6 +11,11 @@ import UIKit
 final class InitialViewController: UIViewController {
     private lazy var tableViewController: UITableView = .init(frame: CGRect.zero, style: .grouped)
 
+    private enum CellsDisplayData: String, CaseIterable {
+        case lab3 = "Лабораторная работа №3"
+        case lab4 = "Лабораторная работа №4"
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableViewController()
@@ -22,27 +27,52 @@ final class InitialViewController: UIViewController {
         NSLayoutConstraint.activate([
             tableViewController.leftAnchor.constraint(equalTo: view.leftAnchor),
             tableViewController.rightAnchor.constraint(equalTo: view.rightAnchor),
-            tableViewController.topAnchor.constraint(equalTo: view.topAnchor),
-            tableViewController.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            tableViewController.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableViewController.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
+        tableViewController.contentInset = UIEdgeInsets(top: 150, left: 0, bottom: 0, right: 0)
     }
 
     private func setupTableViewController() {
         view.addSubview(tableViewController)
+//        view.backgroundColor = .systemGray6
         tableViewController.delegate = self
         tableViewController.dataSource = self
-        tableViewController.backgroundColor = .systemGray6
+        tableViewController.backgroundColor = .systemBackground
+        tableViewController.tableHeaderView = UIView(frame: CGRect(x: 0,
+                                                                   y: 0,
+                                                                   width: tableViewController.frame.width,
+                                                                   height: CGFloat.leastNormalMagnitude))
+        registerCell()
+    }
+
+    private func registerCell() {
+        tableViewController.register(InitialViewControllerCell.self,
+                                     forCellReuseIdentifier: "InitialViewControllerCell")
     }
 }
 
-extension InitialViewController: UITableViewDelegate {}
+extension InitialViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        50
+    }
+
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        <#code#>
+//    }
+}
 
 extension InitialViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        1
+        CellsDisplayData.allCases.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        UITableViewCell()
+        guard let cell = tableViewController.dequeueReusableCell(withIdentifier: "InitialViewControllerCell", for: indexPath) as? InitialViewControllerCell else {
+            return UITableViewCell()
+        }
+        cell.configure(text: CellsDisplayData.allCases[indexPath.row].rawValue)
+
+        return cell
     }
 }
