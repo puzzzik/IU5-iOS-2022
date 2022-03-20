5786668b9b2d15591c5d0af31da6332e3a484bc4
puzzzik <vtazenkovv@yandex.ru>
2022-03-20 15:07:12 +0300

    add lab4 tableViewCell

--- LABS/LAB_4/Table/Lab4TableViewController.swift
+++ LABS/LAB_4/Table/Lab4TableViewController.swift
@@ -22,10 +22,11 @@ final class Lab4TableViewController: UIViewController {
     
     private func setupTableViewController() {
         view.addSubview(tableView)
-        title = "TableView"
-        tableView.backgroundColor = .green
+        title = "Табличечка"
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.backgroundColor = .systemBackground
+        registerCell()
     }
     private func setupTableViewConstraints() {
         tableView.translatesAutoresizingMaskIntoConstraints = false
@@ -34,12 +35,18 @@ final class Lab4TableViewController: UIViewController {
             tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
             tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
             tableView.rightAnchor.constraint(equalTo: view.rightAnchor)])
+    
+    tableView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    private func registerCell() {
+        tableView.register(Lab4TableViewControllerCell.self, forCellReuseIdentifier: "Lab4TableViewControllerCell")
     }
 }
 
 extension Lab4TableViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        50
+        60
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
@@ -47,10 +54,20 @@ extension Lab4TableViewController: UITableViewDelegate {
 }
 extension Lab4TableViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        UITableViewCell()
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Lab4TableViewControllerCell") as? Lab4TableViewControllerCell else {
+            return UITableViewCell()
+        }
+        cell.configure(title: "Main view",
+                       subtitle: "Opens view with UIKit stuff",
+                       image: UIImage(systemName: "doc.circle"))
+        return cell
+                
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
         1
     }
 }
