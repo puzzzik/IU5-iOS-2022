69f230fa5a8d484b3410369a135ac77e237d88dd
puzzzik <vtazenkovv@yandex.ru>
2022-03-20 03:22:29 +0300

    refactoring

--- LABS/LAB_3/ViewController.swift
+++ LABS/LAB_3/ViewController.swift
@@ -8,42 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var TableButton: UIButton!
-    @IBOutlet weak var CollectionButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupTableButton()
-        setupCollectionButton()
-        setupTitle()
     }
-
-
-    private func setupTableButton() {
-        TableButton.setTitle("TableView", for: .normal)
-    }
-    private func setupCollectionButton() {
-        CollectionButton.setTitle("CollectionView", for: .normal)
-    }
-    private func setupTitle() {
-        title = "Main controller"
-    }
-    
-    @IBAction func TapTableButton(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let tableViewController = storyboard.instantiateViewController(identifier: "tableViewController")
-
-        show(tableViewController, sender: self)
-    }
-    
-    @IBAction func TapCollectionButton(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let collectionViewController = storyboard.instantiateViewController(identifier: "collectionViewController")
-
-        show(collectionViewController, sender: self)
-        
-    }
-    
-    
 }
 
